1d03bbec165328ad57a2d713f17bea2daedeebfa
Link4Electronics <link4electronics@gmail.com>
2026-09-08 23:17:26 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -19,7 +19,7 @@ echo "Building NBlood..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/NBlood/NBlood"
 VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
-git clone "$REPO" ./NBlood
+git clone --depth 1 "$REPO" ./NBlood
 echo "$VERSION" > ~/version
 
 mkdir -p ./AppDir/bin
